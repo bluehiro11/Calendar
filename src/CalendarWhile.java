6f9043fa1a1f148c89b39f4013612d65b640efd8
bluehiro11 <bluehiro11@hotmail.com>
2018-06-13 16:28:16 +0900

    Add While but not working, infinite loop...

---
+++ src/CalendarWhile.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class CalendarWhile {
+	private static final int[] MAX_DAYS =  {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int getMaxDaysOfMonth(int month){
+		return MAX_DAYS[month - 1];
+	}
+	
+	public static void main(String args[]){
+		String PROMPT = "cal> ";
+		Scanner scanner = new Scanner(System.in);
+		CalendarWhile cal = new CalendarWhile();
+		
+		int month = 1;
+		
+		while(true){
+			System.out.println("월을 입력하세요");
+			System.out.print(PROMPT);
+			if(month == -1){
+				break;
+			}
+			if(month > 12){
+				continue;
+			}
+			System.out.printf("%d 월은 %d 일입니다", month, cal.getMaxDaysOfMonth(month));
+			}
+		System.out.println("Bye~~");
+		scanner.close();
+	}
+	
+}
